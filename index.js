f1c4283aa28983823614c75c785e484fddf253b4
digitalronin <david@digitalronin.com>
2022-02-12 12:05:19 +0900

    Store and retrieve some data
    Store some data, and use its unique reference to
    retrieve it again.

--- index.js
+++ index.js
@@ -2,4 +2,20 @@ const IPFS = require('ipfs');
 
 (async () => {
   const node = await IPFS.create();
+
+  const stored = await node.add({
+    path: 'hello.txt',
+    content: Buffer.from('Hello World 101')
+  });
+
+  console.log(stored);
+
+  const ref = stored.cid.toString();
+  console.log(ref);
+
+  const retrieved = await node.cat(ref);
+  const decoder = new TextDecoder()
+  for await (const chunk of retrieved) {
+    console.log(decoder.decode(chunk))
+  }
 })();
